0f5b43290f1ddec08e7c239653a3e890a7a34d73
Omar Torres <omar.torres@fueled.com>
2024-07-19 22:03:05 -0500

    Show message when there are no explorations encouraging users to create their first. And split UI into different widgets in ExplorationsView.

--- lib/views/explorations_view.dart
+++ lib/views/explorations_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import '../models/exploration.dart';
 import 'exploration_details_view.dart';
 import 'exploration_item_view.dart';
+import 'new_exploration_view.dart';
 
 class ExplorationsView extends StatefulWidget {
   const ExplorationsView({super.key});
@@ -36,36 +38,100 @@ class ExplorationsViewState extends State<ExplorationsView> {
     });
   }
 
+  void fetchNewExplorations(String result) async {
+    if (result == 'success') {
+      setState(() {
+        explorations = fetchExplorations();
+      });
+      Fluttertoast.showToast(
+        msg: "Your exploration has been created!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.TOP,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 247, 247, 247),
-        body: FutureBuilder<List<Exploration>>(
-            future: explorations,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return ExplorationItemView(
-                      exploration: snapshot.data![index],
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ExplorationDetailsView(
-                                    exploration: snapshot.data![index],
-                                  )),
-                        );
-                      },
-                    );
-                  },
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
+      backgroundColor: Color.fromARGB(255, 247, 247, 247),
+      body: FutureBuilder<List<Exploration>>(
+          future: explorations,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data!.length == 9) {
+                return createFirstExplorationWidget();
               } else {
-                return const CircularProgressIndicator();
+                return explorationsWidget(snapshot);
               }
-            }));
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            } else {
+              return const CircularProgressIndicator();
+            }
+          }
+        )
+      );
+  }
+
+  Widget explorationsWidget(AsyncSnapshot snapshot) {
+    return ListView.builder(
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        return ExplorationItemView(
+          exploration: snapshot.data![index],
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ExplorationDetailsView(
+                        exploration: snapshot.data![index],
+                      )),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget createFirstExplorationWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("Create your first exploration"),
+          const SizedBox(height: 0),
+          Container(
+            padding: EdgeInsets.zero, // removes padding
+            margin: EdgeInsets.zero, // removes margin
+            child: ElevatedButton(
+              onPressed: () async {
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NewExplorationView()),
+                );
+                fetchNewExplorations(result);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 53, 53, 53),
+                foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                shape: const CircleBorder(),
+                fixedSize: Size(20, 20), // width and height
+                padding: EdgeInsets.zero,
+              ),
+              child: const Icon(
+                Icons.add,
+                size: 15, // resize the icon
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
